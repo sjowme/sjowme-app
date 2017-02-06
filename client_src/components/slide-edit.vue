c874e86d5b0ba93de845413be2421bba680d510f
sjowme <andre@sjow.me>
2017-02-06 16:05:04 +0100

    Added css. If & Or statement by class active for info-icon

--- client_src/components/slide-edit.vue
+++ client_src/components/slide-edit.vue
@@ -260,7 +260,7 @@
                                             <li v-for="mod in modules"
                                                 :class="{'uk-active': current_module.name == mod.name}">
                                                 <a @click="setModule(mod)">{{mod.label}}</a>
-												<div :class="{'uk-active': mod.name == 'twitter'}">
+												<div :class="{'uk-active': mod.name == 'twitter' || mod.name == 'weather' || mod.name == 'rss'}">
 													<info-icon
 														:title="$trans('U kunt maar 1x deze module gebruiken per sjow.')"></info-icon>
 												</div>

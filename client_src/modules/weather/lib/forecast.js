dd782668d6be8a62b3230aece15973734fb5143b
sjowme <andre@sjow.me>
2017-02-06 16:05:04 +0100

    Removed id from getForecaster().get(

--- client_src/modules/weather/lib/forecast.js
+++ client_src/modules/weather/lib/forecast.js
@@ -24,7 +24,7 @@ module.exports = {
 
     getWeather(units, lat, lng, ignore_cache, id, cb) {
         // Retrieve weather information from coordinates
-        getForecaster(units).get([lat, lng], ignore_cache || false, id, (err, weather) => {
+        getForecaster(units).get([lat, lng], ignore_cache || false, (err, weather) => {
             if(err) {
                 return cb(err);
             }
